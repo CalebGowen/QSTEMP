b9655ceb68dbc22beb022608eeeedf1093cef0b9
Caleb Gowen <cgowen@qualservcorp.com>
2022-08-09 14:52:51 -0500

    php test

---
+++ ticketForm.php
@@ -0,0 +1,20 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require '../PHPMailer/src/Exception.php';
+require '../PHPMailer/src/PHPMailer.php';
+require '../PHPMailer/src/SMTP.php';
+
+if (isset($_POST["ticket"])){
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $subject = $_POST["subject"];
+    $message = $_POST["message"];
+    
+    echo $name;
+}
+
+echo "hello";
+?>
